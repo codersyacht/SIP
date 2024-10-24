3e907f044e9859204f304c9a34f8071d709534ec
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-24 12:39:01 +0530

    06.image_pull_secret.sh

--- 6.image_pull_secret.sh
+++
@@ -1,3 +0,0 @@
-export ENTITLEDKEY=$(cat ibm-entitlement-key)
-export NAMESPACE="sip"
-kubectl create secret docker-registry ibm-entitlement-key --docker-server=cp.icr.io --docker-username=cp --docker-password=${ENTITLEDKEY} --namespace=${NAMESPACE}
